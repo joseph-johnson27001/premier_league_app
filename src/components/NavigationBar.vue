7b5fe77312d3e5c1f0baa52b31287c506571d513
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-08-30 17:47:24 +0100

    Navigation bar location

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,35 +1,38 @@
 <template>
   <nav class="navigation-bar">
-    <router-link
-      to="/"
-      class="nav-link"
-      :class="{ 'active-link': isRouteActive('/') }"
-      >Home</router-link
-    >
-    <router-link
-      to="/table"
-      class="nav-link"
-      :class="{ 'active-link': isRouteActive('/table') }"
-      >Table</router-link
-    >
-    <router-link
-      to="/fixtures"
-      class="nav-link"
-      :class="{ 'active-link': isRouteActive('/fixtures') }"
-      >Fixtures</router-link
-    >
-    <router-link
-      to="/results"
-      class="nav-link"
-      :class="{ 'active-link': isRouteActive('/results') }"
-      >Results</router-link
-    >
-    <router-link
-      to="/news"
-      class="nav-link"
-      :class="{ 'active-link': isRouteActive('/news') }"
-      >News</router-link
-    >
+    <div class="navigation-container">
+      <router-link
+        to="/"
+        class="nav-link"
+        :class="{ 'active-link': isRouteActive('/') }"
+        id="home-button"
+        >Home</router-link
+      >
+      <router-link
+        to="/table"
+        class="nav-link"
+        :class="{ 'active-link': isRouteActive('/table') }"
+        >Table</router-link
+      >
+      <router-link
+        to="/fixtures"
+        class="nav-link"
+        :class="{ 'active-link': isRouteActive('/fixtures') }"
+        >Fixtures</router-link
+      >
+      <router-link
+        to="/results"
+        class="nav-link"
+        :class="{ 'active-link': isRouteActive('/results') }"
+        >Results</router-link
+      >
+      <router-link
+        to="/news"
+        class="nav-link"
+        :class="{ 'active-link': isRouteActive('/news') }"
+        >News</router-link
+      >
+    </div>
   </nav>
 </template>
 
@@ -45,26 +48,33 @@ export default {
 </script>
 
 <style scoped>
-/* Custom styles for navy blue navigation bar */
 .navigation-bar {
   display: flex;
-  justify-content: center;
-  background-color: #004080; /* Navy blue background */
+  background-color: #004080;
   padding: 10px 0;
+  justify-content: center;
+}
+
+.navigation-container {
+  width: 1200px;
 }
 
 .nav-link {
-  color: white; /* White text color */
+  color: white;
   text-decoration: none;
   margin: 0 15px;
   transition: color 0.3s;
 }
 
+#home-button {
+  margin-left: 0px;
+}
+
 .nav-link:hover {
-  color: #ffd700; /* Gold color on hover */
+  color: #ffd700;
 }
 
 .active-link {
-  color: #ffd700; /* Gold color for active link */
+  color: #ffd700;
 }
 </style>

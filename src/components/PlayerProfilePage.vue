fe00e908f90a4ebb5efc0360ca2c58dd2376f522
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-08-31 13:39:36 +0100

    PlayerProfilePage bringing back data

--- src/components/PlayerProfilePage.vue
+++ src/components/PlayerProfilePage.vue
@@ -1,74 +1,74 @@
 <template>
-  <div class="player-profile">
-    <div v-if="loading">
-      <p>Loading player data...</p>
+  <div>
+    <div v-if="loading" class="loading-indicator">
+      <p>Loading...</p>
     </div>
-    <div class="player-profile" v-else>
-      <div
-        class="hero"
-        :style="{
-          backgroundColor: player.currentTeam.clubColors.split(' / ')[0],
-        }"
-      >
-        <div class="hero-overlay">
-          <h1
-            class="hero-title"
-            :style="{ color: player.currentTeam.clubColors.split(' / ')[1] }"
-          >
-            {{ player.name }}
-          </h1>
-        </div>
-      </div>
-      <div class="player-details">
-        <img
-          :src="player.currentTeam.crest"
-          :alt="player.currentTeam.name"
-          class="player-image"
-        />
-        <div class="player-info">
-          <p class="player-position">{{ player.position }}</p>
-          <p class="player-birth">{{ player.dateOfBirth }}</p>
-          <p class="player-nationality">{{ player.nationality }}</p>
-        </div>
-      </div>
-      <h2 class="section-title">Current Team</h2>
-      <div class="team-details">
-        <div class="team-info">
+    <div v-else>
+      <div class="team-page">
+        <div class="team-header">
           <img
             :src="player.currentTeam.crest"
-            :alt="player.currentTeam.name"
-            class="team-crest"
+            :alt="player.currentTeam.name + ' Crest'"
           />
-          <div class="team-text">
-            <p class="team-name">{{ player.currentTeam.name }}</p>
-            <p class="team-address">{{ player.currentTeam.address }}</p>
-            <p class="team-website">
-              <a :href="player.currentTeam.website" target="_blank">{{
-                player.currentTeam.website
-              }}</a>
+          <div class="team-info">
+            <h1 class="team-name">
+              {{ player.name }} [{{ player.shirtNumber }}]
+            </h1>
+            <p class="team-manager">
+              Position: {{ player.position }} ({{ player.nationality }})
             </p>
+            <p class="team-founded">Date of Birth: {{ player.dateOfBirth }}</p>
           </div>
         </div>
-        <div class="team-extra">
-          <p class="team-founded">{{ player.currentTeam.founded }}</p>
-          <p class="team-colors">{{ player.currentTeam.clubColors }}</p>
-          <p class="team-venue">{{ player.currentTeam.venue }}</p>
+        <div class="player-info">
+          <h2 class="info-title">Player Information</h2>
+          <div class="player-contract">
+            <p class="contract-dates">
+              Contract: {{ player.currentTeam.contract.start }} -
+              {{ player.currentTeam.contract.until }}
+            </p>
+          </div>
+
+          <!-- Include the rest of the player's information here -->
+          <div class="recent-matches">
+            <h2>Recent Matches</h2>
+            <div v-for="match in matches" :key="match.id" class="match">
+              <div class="match-header">
+                <img
+                  :src="match.competition.emblem"
+                  :alt="match.competition.name + ' Emblem'"
+                />
+                <p class="match-competition">{{ match.competition.name }}</p>
+                <p class="match-date">{{ formatDate(match.utcDate) }}</p>
+                <p class="match-status">{{ match.status }}</p>
+              </div>
+              <div class="match-details" v-if="match.status === 'FINISHED'">
+                <p class="match-result">
+                  Result: {{ match.score.fullTime.home }} -
+                  {{ match.score.fullTime.away }}
+                  {{
+                    match.score.winner === "HOME_TEAM"
+                      ? "(Home Team)"
+                      : match.score.winner === "AWAY_TEAM"
+                      ? "(Away Team)"
+                      : "(Draw)"
+                  }}
+                </p>
+                <p class="match-venue">Venue: {{ match.venue }}</p>
+                <p class="match-attendance">
+                  Attendance: {{ match.attendance }}
+                </p>
+                <p class="match-referees">
+                  Referee: {{ match.referees[0].name }} ({{
+                    match.referees[0].nationality
+                  }})
+                </p>
+              </div>
+              <!-- Display more match statistics and team information here -->
+            </div>
+          </div>
         </div>
       </div>
-      <h2 class="section-title">Running Competitions</h2>
-      <ul class="competition-list">
-        <li
-          v-for="competition in player.currentTeam.runningCompetitions"
-          :key="competition.id"
-        >
-          <img
-            :src="competition.emblem"
-            :alt="competition.name"
-            class="competition-emblem"
-          />
-          <p class="competition-name">{{ competition.name }}</p>
-        </li>
-      </ul>
     </div>
   </div>
 </template>
@@ -81,12 +81,14 @@ export default {
   data() {
     return {
       player: {},
-      loading: true, // Add loading state
+      loading: true,
+      matches: [], // Add this array to store the player's match history
     };
   },
   async created() {
-    await this.fetchPlayerProfile(); // Await the data fetch
-    this.loading = false; // Set loading state to false after fetching data
+    await this.fetchPlayerProfile();
+    await this.fetchPlayerMatches(); // Fetch the match history after fetching player profile
+    this.loading = false;
   },
   methods: {
     async fetchPlayerProfile() {
@@ -98,139 +100,70 @@ export default {
         console.error("Error fetching player profile:", error);
       }
     },
+    async fetchPlayerMatches() {
+      const playerId = this.$route.params.id;
+      try {
+        const response = await axios.get(`/api/persons/${playerId}/matches`);
+        this.matches = response.data.matches; // Store the match history in the 'matches' array
+        console.log(this.matches);
+      } catch (error) {
+        console.error("Error fetching player matches:", error);
+      }
+    },
+    formatDate(date) {
+      return new Date(date).toLocaleDateString();
+    },
   },
 };
 </script>
 
 <style scoped>
-/* Add your styles here */
-.player-profile {
+.team-page {
+  font-family: "Arial", sans-serif;
+  background-color: #f5f5f5;
   padding: 20px;
 }
 
-.hero {
-  position: relative;
-  overflow: hidden;
-  height: 300px;
-}
-
-.hero-background {
-  width: 100%;
-  height: auto;
-  object-fit: cover;
-}
-
-.hero-overlay {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
+.team-header {
+  background-color: #333;
   color: white;
-}
-
-.hero-title {
-  font-family: "Montserrat", sans-serif;
-  font-size: 36px;
-  font-weight: bold;
-  margin-bottom: 10px;
-}
-
-.player-details {
   display: flex;
   align-items: center;
-  margin-top: 20px;
-}
-
-.player-image {
-  width: 120px;
-  height: auto;
-  border-radius: 50%;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-}
-
-.player-info {
-  margin-left: 20px;
-}
-
-.player-position {
-  font-size: 18px;
-  font-weight: bold;
-}
-
-.player-birth,
-.player-nationality {
-  font-size: 14px;
-  color: #777;
-}
-
-.section-title {
-  font-family: "Montserrat", sans-serif;
-  font-size: 24px;
-  font-weight: bold;
-  margin-top: 30px;
-}
-
-.team-details {
-  display: flex;
-  margin-top: 20px;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+  margin: -20px -20px 0px -20px;
 }
 
 .team-info {
-  display: flex;
-  align-items: center;
-}
-
-.team-crest {
-  width: 80px;
-  height: auto;
-  margin-right: 20px;
-}
-
-.team-text {
-  margin-left: 20px;
+  flex: 1;
 }
 
 .team-name {
-  font-size: 18px;
+  font-family: "Montserrat", sans-serif;
+  font-size: 32px;
   font-weight: bold;
+  margin: 0;
 }
 
-.team-address,
-.team-website {
-  font-size: 14px;
-  color: #777;
+.team-manager,
+.team-founded {
+  font-size: 16px;
+  margin: 5px 0;
+  color: #aaa;
 }
 
-.team-extra {
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  justify-content: center;
+.category-title,
+.team-manager,
+.team-founded {
+  color: #333;
 }
 
-.team-founded,
-.team-colors,
-.team-venue {
-  font-size: 14px;
-  color: #777;
-}
+/* Additional Media Queries for Ultra-small Devices */
 
-.competition-list {
-  display: flex;
-  list-style: none;
-  margin-top: 20px;
-  padding: 0;
-}
-
-.competition-emblem {
-  width: 30px;
-  height: auto;
-  margin-right: 10px;
-}
-
-.competition-name {
-  font-size: 14px;
+@media (max-width: 360px) {
+  .team-name {
+    font-size: 28px;
+  }
 }
 </style>

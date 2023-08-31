e56e251d567dc6543905d4ab4bd86d5a01335bf9
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-08-31 10:01:59 +0100

    Team page icons and cards

--- src/views/TeamsPage.vue
+++ src/views/TeamsPage.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <h1>Teams</h1>
-    <div class="team-dropdown" v-if="!selectedTeamId">
-      <label for="team">Select Team:</label>
-      <select id="team" v-model="selectedTeamId" @change="fetchTeamPlayers">
-        <option value="">All Teams</option>
-        <option v-for="team in teams" :key="team.id" :value="team.id">
-          {{ team.name }}
-        </option>
-      </select>
+    <div class="team-cards">
+      <div
+        v-for="team in teams"
+        :key="team.id"
+        class="team-card"
+        @click="fetchTeamPlayers(team.id)"
+      >
+        <img :src="team.crest" :alt="team.name" class="team-crest" />
+        <p class="team-name">{{ team.name }}</p>
+      </div>
     </div>
-    <ul v-else>
+    <ul v-if="players.length">
       <li v-for="player in players" :key="player.id">
         <router-link
           :to="{ name: 'player-profile', params: { id: player.id } }"
@@ -37,14 +39,17 @@ export default {
     async fetchTeams() {
       try {
         const response = await axios.get("/api/competitions/PL/teams");
-        this.teams = response.data.teams;
+        this.teams = response.data.teams.map((team) => ({
+          ...team,
+          crest: team.crest, // Use the 'crest' property directly
+        }));
       } catch (error) {
         console.error("Error fetching teams:", error);
       }
     },
-    async fetchTeamPlayers() {
+    async fetchTeamPlayers(teamId) {
       try {
-        const response = await axios.get(`/api/teams/${this.selectedTeamId}`);
+        const response = await axios.get(`/api/teams/${teamId}`);
         this.players = response.data.squad;
       } catch (error) {
         console.error("Error fetching team players:", error);
@@ -54,7 +59,7 @@ export default {
   created() {
     this.fetchTeams();
     if (this.selectedTeamId) {
-      this.fetchTeamPlayers();
+      this.fetchTeamPlayers(this.selectedTeamId);
     }
   },
 };
@@ -62,4 +67,34 @@ export default {
 
 <style scoped>
 /* Add your styles here */
+.team-cards {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+
+.team-card {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 15px;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  transition: background-color 0.3s;
+}
+
+.team-card:hover {
+  background-color: #f2f2f2;
+}
+
+.team-crest {
+  width: 100px;
+  height: 100px;
+}
+
+.team-name {
+  margin-top: 10px;
+  text-align: center;
+}
 </style>

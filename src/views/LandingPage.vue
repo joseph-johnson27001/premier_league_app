12a6b212774296381f4a2ead7faeb51c7f7e0713
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-03-13 11:17:14 +0700

    Generic selection box

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,5 +1,35 @@
 <template>
-    <div>
-        Landing page goes here
-    </div>
+  <div>
+    <p>Please select your favorite team:</p>
+    <select v-model="selectedTeam">
+      <option value="">Select a team...</option>
+      <option v-for="team in teams" :key="team.id" :value="team.id">
+        {{ team.name }}
+      </option>
+    </select>
+    <button @click="next">Next</button>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedTeam: "",
+      teams: [
+        { id: 1, name: "Team A" },
+        { id: 2, name: "Team B" },
+        { id: 3, name: "Team C" },
+      ],
+    };
+  },
+  methods: {
+    next() {
+      // Perform actions when the "Next" button is clicked
+      console.log("Selected Team:", this.selectedTeam);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
